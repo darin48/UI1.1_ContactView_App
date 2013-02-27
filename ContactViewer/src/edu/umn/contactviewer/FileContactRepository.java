ff081616e439e967f328a500d7899394ab4b0824
ward0093 <david.ward@capmation.com>
2013-02-26 23:19:57 -0600

    Updated for Kevin

--- ContactViewer/src/edu/umn/contactviewer/FileContactRepository.java
+++ ContactViewer/src/edu/umn/contactviewer/FileContactRepository.java
@@ -13,89 +13,91 @@ import android.content.Context;
 import com.google.gson.Gson;
 
 public class FileContactRepository implements ContactRepository, Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	private int nextID = 1;
-	private String fileName = null;
-	private Context context = null;
-	private Map<Integer, Contact> contacts = new HashMap<Integer, Contact>();
+    private static final long serialVersionUID = 1L;
 
-	public FileContactRepository() {
-	}
+    private int nextID = 1;
+    private String fileName = null;
+    private Map<Integer, Contact> contacts = new HashMap<Integer, Contact>();
 
-	public void connect(Context context, String fileName) {
-		FileInputStream fis = null;
-		this.fileName = fileName;
-		this.context = context;
-		try {
-			fis = context.openFileInput(fileName);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			String gsonContacts = (String) ois.readObject();
-			Gson gson = new Gson();
-			nextID = gson.fromJson(gsonContacts, int.class);
-			Contact[] readContacts = gson.fromJson(gsonContacts,
-					Contact[].class);
-			for (int i = 0; i < readContacts.length; ++i)
-				contacts.put(new Integer(readContacts[i].getID()),
-						readContacts[i]);
-		} catch (Exception e) {
-		}
-	}
+    public FileContactRepository(String fileName) {
+        this.fileName = fileName;
+    }
 
-	@Override
-	public Contact newContact() {
-		int id = nextID++;
-		Contact result = new Contact(id);
-		contacts.put(new Integer(id), result);
-		return result;
-	}
+    @Override
+    public void connect(Context context) {
+        FileInputStream fis = null;
+        try {
+            fis = context.openFileInput(fileName);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            String gsonContacts = (String) ois.readObject();
+            Gson gson = new Gson();
+            Contact[] readContacts = gson.fromJson(gsonContacts,
+                    Contact[].class);
+            int maxID = readContacts[0].getID();
+            for (int i = 0; i < readContacts.length; ++i) {
+                Contact contact = readContacts[i];
+                int id = contact.getID();
+                if (maxID < id)
+                    maxID = id;
+                contacts.put(new Integer(id), contact);
+            }
+            nextID = maxID + 1;
+        } catch (Exception e) {
+        }
+    }
 
-	@Override
-	public Contact lookupContact(int id) {
-		Contact result = contacts.get(new Integer(id));
-		return result;
-	}
+    @Override
+    public Contact newContact() {
+        int id = nextID++;
+        Contact result = new Contact(id);
+        contacts.put(new Integer(id), result);
+        return result;
+    }
 
-	@Override
-	public void deleteContact(int id) {
-		contacts.remove(new Integer(id));
-	}
+    @Override
+    public Contact lookupContact(int id) {
+        Contact result = contacts.get(new Integer(id));
+        return result;
+    }
 
-	@Override
-	public int[] getAllContacts() {
-		int[] result = new int[contacts.size()];
-		int n = 0;
-		for (Integer key : contacts.keySet()) {
-			result[n++] = key.intValue();
-		}
-		return result;
-	}
+    @Override
+    public void deleteContact(int id) {
+        contacts.remove(new Integer(id));
+    }
 
-	@Override
-	public Contact[] getContacts(int[] ids) {
-		Contact[] result = new Contact[ids.length];
-		for (int i = 0; i < ids.length; ++i) {
-			result[i] = lookupContact(ids[i]);
-		}
-		return result;
-	}
+    @Override
+    public int[] getAllContacts() {
+        int[] result = new int[contacts.size()];
+        int n = 0;
+        for (Integer key : contacts.keySet()) {
+            result[n++] = key.intValue();
+        }
+        return result;
+    }
 
-	@Override
-	public void flush() {
-		Contact[] array = new Contact[contacts.size()];
-		contacts.values().toArray(array);
-		Gson gson = new Gson();
-		String gsonNextID = gson.toJson(nextID);
-		String gsonContacts = gson.toJson(array);
-		FileOutputStream fos;
-		try {
-			fos = context.openFileOutput(fileName, Context.MODE_PRIVATE);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(gsonNextID);
-			oos.writeObject(gsonContacts);
-		} catch (Exception exc) {
-			throw new RuntimeException("Cannot store contacts", exc);
-		}
-	}
+    @Override
+    public Contact[] getContacts(int[] ids) {
+        Contact[] result = new Contact[ids.length];
+        for (int i = 0; i < ids.length; ++i) {
+            result[i] = lookupContact(ids[i]);
+        }
+        return result;
+    }
+
+    @Override
+    public void flush(Context context) {
+        Contact[] array = new Contact[contacts.size()];
+        contacts.values().toArray(array);
+        Gson gson = new Gson();
+        String gsonContacts = gson.toJson(array);
+        FileOutputStream fos;
+        try {
+            fos = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(gsonContacts);
+        } catch (Exception exc) {
+            throw new RuntimeException("Cannot store contacts", exc);
+        }
+    }
 
 }
